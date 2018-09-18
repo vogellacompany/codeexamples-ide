c522746c422cdaa971159e983833b0152d2a72eb
Lars Vogel <Lars.Vogel@vogella.com>
2018-09-18 12:36:22 +0200

    Adding property change support for Todo

--- com.vogella.tasks.model/src/com/vogella/tasks/model/Todo.java
+++ com.vogella.tasks.model/src/com/vogella/tasks/model/Todo.java
@@ -1,32 +1,35 @@
 package com.vogella.tasks.model;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 import java.util.Date;
-import java.util.Objects;
-
-import org.eclipse.core.resources.IResource;
 
 public class Todo {
 
-	public static final String FIELD_RESOURCE = "resource";
+	private PropertyChangeSupport changes = new PropertyChangeSupport(this);
+
+	public static final String FIELD_ID = "id";
+	public static final String FIELD_SUMMARY = "summary";
+	public static final String FIELD_DESCRIPTION = "description";
+	public static final String FIELD_DONE = "done";
+	public static final String FIELD_DUEDATE = "dueDate";
 
 	private final long id;
-    private String summary = "";
-    private String description = "";
+	private String summary = "";
+	private String description = "";
     private boolean done = false;
     private Date dueDate = new Date();
-	private IResource resource;
 
-    public Todo(long id) {
-        this.id = id;
+	public Todo(long i) {
+		id = i;
     }
 
-    public Todo(long id, String summary, String description, boolean done, Date dueDate) {
-        this.id = id;
+	public Todo(long i, String summary, String description, boolean b, Date date) {
+		this.id = i;
         this.summary = summary;
         this.description = description;
-        this.done = done;
-        setDueDate(dueDate);
-
+		this.done = b;
+		this.dueDate = date;
     }
 
     public long getId() {
@@ -38,7 +41,7 @@ public class Todo {
     }
 
     public void setSummary(String summary) {
-        this.summary = summary;
+		changes.firePropertyChange(FIELD_SUMMARY, this.summary, this.summary = summary);
     }
 
     public String getDescription() {
@@ -46,15 +49,15 @@ public class Todo {
     }
 
     public void setDescription(String description) {
-        this.description = description;
+		changes.firePropertyChange(FIELD_DESCRIPTION, this.description, this.description = description);
     }
 
     public boolean isDone() {
         return done;
     }
 
-    public void setDone(boolean done) {
-        this.done = done;
+	public void setDone(boolean isDone) {
+		changes.firePropertyChange(FIELD_DONE, this.done, this.done = isDone);
     }
 
     public Date getDueDate() {
@@ -62,25 +65,17 @@ public class Todo {
     }
 
     public void setDueDate(Date dueDate) {
-        this.dueDate = new Date(dueDate.getTime());
-    }
-
-	public IResource getResource() {
-		return resource;
+		changes.firePropertyChange(FIELD_DUEDATE, this.dueDate, this.dueDate = new Date(dueDate.getTime()));
 	}
 
-	public void setResource(IResource resource) {
-		this.resource = resource;
-	}
 	@Override
 	public int hashCode() {
-		return Objects.hash(description, done, dueDate, id, summary);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
 	}
-	public Todo copy() {
-	    return new Todo(this.id, this.summary,
-	            this.description, this.done,
-	            getDueDate());
-	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -90,13 +85,25 @@ public class Todo {
 		if (getClass() != obj.getClass())
 			return false;
 		Todo other = (Todo) obj;
-		return Objects.equals(description, other.description) && done == other.done
-				&& Objects.equals(dueDate, other.dueDate) && id == other.id && Objects.equals(summary, other.summary);
+		if (id != other.id)
+			return false;
+		return true;
 	}
 
 	@Override
 	public String toString() {
-		return "Todo [id=" + id + ", summary=" + summary + ", description=" + description + ", done=" + done
-				+ ", dueDate=" + dueDate + "]";
+		return "Todo [id=" + id + ", summary=" + summary + "]";
+    }
+
+	public Todo copy() {
+		return new Todo(id, summary, description, done, getDueDate());
+	}
+
+	public void addPropertyChangeListener(PropertyChangeListener l) {
+		changes.addPropertyChangeListener(l);
+	}
+
+	public void removePropertyChangeListener(PropertyChangeListener l) {
+		changes.removePropertyChangeListener(l);
 	}
 }
