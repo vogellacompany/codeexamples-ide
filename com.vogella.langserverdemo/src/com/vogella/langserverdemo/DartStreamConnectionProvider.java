d0e55c93a4121e96ee5b56fd3da8934b2fd4e4b6
Jonas Hungershausen <jonas.hungershausen@vogella.com>
2019-01-31 19:00:03 +0100

    [LSP] Use the new language server from the nightly build

--- com.vogella.langserverdemo/src/com/vogella/langserverdemo/DartStreamConnectionProvider.java
+++ com.vogella.langserverdemo/src/com/vogella/langserverdemo/DartStreamConnectionProvider.java
@@ -1,13 +1,21 @@
 package com.vogella.langserverdemo;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.eclipse.lsp4e.server.ProcessStreamConnectionProvider;
 
 public class DartStreamConnectionProvider extends ProcessStreamConnectionProvider {
 
 	public DartStreamConnectionProvider() {
-		setWorkingDirectory(System.getProperty("user.dir"));
-		setCommands(Arrays.asList("/usr/bin/env", ".pub-cache/bin/dart_language_server"));
+		String userDir = System.getProperty("user.dir");
+		String dartLocation = userDir + "/Library/dart-nightly/bin";
+		setWorkingDirectory(userDir);
+		List<String> commands = Arrays.asList(
+				dartLocation + "/dart", // Use the dart executable
+				dartLocation + "/snapshots/analysis_server.dart.snapshot", // The language server
+				"--lsp" // Needed parameter to tell the server that it should use the LSP
+		);
+		setCommands(commands);
 	}
 }
