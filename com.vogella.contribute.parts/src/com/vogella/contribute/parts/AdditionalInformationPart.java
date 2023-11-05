854e24f0fb7a14a45152cab6afc218553cfe4992
Lars Vogel <Lars.Vogel@vogella.com>
2023-11-05 23:03:03 +0100

    Update of contribution classpath and pde spies in the target

--- com.vogella.contribute.parts/src/com/vogella/contribute/parts/AdditionalInformationPart.java
+++ com.vogella.contribute.parts/src/com/vogella/contribute/parts/AdditionalInformationPart.java
@@ -1,28 +1,14 @@
 package com.vogella.contribute.parts;
 
-import static org.eclipse.jface.databinding.swt.typed.WidgetProperties.text;
-
 import javax.annotation.PostConstruct;
 
-import org.eclipse.core.databinding.DataBindingContext;
-import org.eclipse.jface.databinding.swt.ISWTObservableValue;
+import org.eclipse.jface.widgets.TextFactory;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Text;
 
 public class AdditionalInformationPart {
-	@PostConstruct
-	public void postConstruct(Composite parent) {
-		Text modelText = new Text(parent, SWT.BORDER | SWT.MULTI);
-		Text targetText = new Text(parent, SWT.BORDER | SWT.MULTI);
-		DataBindingContext db = new DataBindingContext();
-		ISWTObservableValue<String> modelObserve = text(SWT.Modify).observeDelayed(5000, modelText);
-		ISWTObservableValue<String> targetObserve = text(SWT.Modify).observe(targetText);
-		db.bindValue(targetObserve, modelObserve);
-		modelText.setText("Morning");
-		targetText.setText("Evening");
-
-//		modelText.addModifyListener(e -> targetText.setText(modelText.getText()));
-//		targetText.addModifyListener(e -> modelText.setText(targetText.getText()));
-	}
+    @PostConstruct
+    public void postConstruct(Composite parent) {
+        TextFactory.newText(SWT.BORDER |SWT.MULTI).create(parent);
+    }
 }
