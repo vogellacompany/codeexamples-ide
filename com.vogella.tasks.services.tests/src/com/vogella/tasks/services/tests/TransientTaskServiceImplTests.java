fb23dffc57f4e82c13323501ffe30f893d82ed89
Lars Vogel <Lars.Vogel@vogella.com>
2024-11-20 13:52:27 +0100

    Remove unnecessary import

--- com.vogella.tasks.services.tests/src/com/vogella/tasks/services/tests/TransientTaskServiceImplTests.java
+++ com.vogella.tasks.services.tests/src/com/vogella/tasks/services/tests/TransientTaskServiceImplTests.java
@@ -1,6 +1,5 @@
 package com.vogella.tasks.services.tests;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
