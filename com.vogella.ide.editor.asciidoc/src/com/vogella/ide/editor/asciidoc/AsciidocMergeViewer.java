5c74d7413a4457ea058a82c4c8f226a092812b93
Lars Vogel <Lars.Vogel@vogella.com>
2025-11-18 09:12:05 +0100

    Fix: Restore custom colors in AsciidocMergeViewer
    This commit fixes a regression introduced in the previous commit. The functionality for setting custom colors in the AsciidocMergeViewer was incorrectly removed. This change restores the functionality by using the constants from ICompareUIConstants.

--- com.vogella.ide.editor.asciidoc/src/com/vogella/ide/editor/asciidoc/AsciidocMergeViewer.java
+++ com.vogella.ide.editor.asciidoc/src/com/vogella/ide/editor/asciidoc/AsciidocMergeViewer.java
@@ -1,12 +1,16 @@
 package com.vogella.ide.editor.asciidoc;
 
 import org.eclipse.compare.CompareConfiguration;
+import org.eclipse.compare.ICompareUIConstants;
 import org.eclipse.compare.contentmergeviewer.TextMergeViewer;
+import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.widgets.Composite;
 
 public class AsciidocMergeViewer extends TextMergeViewer {
 
     public AsciidocMergeViewer(Composite parent, int style, CompareConfiguration configuration) {
         super(parent, style, configuration);
+        configuration.setProperty(ICompareUIConstants.INCOMING_COLOR, new RGB(0, 255, 0));
+        configuration.setProperty(ICompareUIConstants.OUTGOING_COLOR, new RGB(255, 0, 0));
     }
 }
