7a2aa858faffe541b91ad517ad6b1b9be4338e60
Lars Vogel <Lars.Vogel@vogella.com>
2025-11-18 11:55:55 +0100

    Fixes: Address review feedback for PR 48

--- com.vogella.ide.editor.asciidoc/src/com/vogella/ide/editor/asciidoc/util/AsciiDocResourceUtil.java
+++ com.vogella.ide.editor.asciidoc/src/com/vogella/ide/editor/asciidoc/util/AsciiDocResourceUtil.java
@@ -1,8 +1,11 @@
 package com.vogella.ide.editor.asciidoc.util;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.PlatformUI;
@@ -10,16 +13,23 @@ import org.eclipse.ui.PlatformUI;
 public class AsciiDocResourceUtil {
 
     public static IContainer getParentFolder() {
-        IEclipseContext context = PlatformUI.getWorkbench().getService(IEclipseContext.class);
-        Object object = context.get("activeEditor");
+        // Use AtomicReference to hold the result from the UI thread
+        final AtomicReference<IContainer> containerRef = new AtomicReference<>();
 
-        if (object instanceof IEditorPart activeEditor) {
-            IEditorInput editorInput = activeEditor.getEditorInput();
-            IResource adapter = editorInput.getAdapter(IResource.class);
-            if (adapter != null) {
-                return adapter.getParent();
+        // Ensure UI-related code runs on the UI thread
+        Display.getDefault().syncExec(() -> {
+            IEclipseContext context = PlatformUI.getWorkbench().getService(IEclipseContext.class);
+            Object object = context.get("activeEditor");
+
+            if (object instanceof IEditorPart activeEditor) {
+                IEditorInput editorInput = activeEditor.getEditorInput();
+                IResource adapter = editorInput.getAdapter(IResource.class);
+                if (adapter != null) {
+                    containerRef.set(adapter.getParent());
+                }
             }
-        }
-        return null;
+        });
+
+        return containerRef.get();
     }
 }
